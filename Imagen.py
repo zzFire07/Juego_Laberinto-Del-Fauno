26e890421e4c5f0fe33ab464ecb66e1f22e8a2ae
yazminEspagnolo <104398358+yazminEspagnolo@users.noreply.github.com>
2022-05-24 19:54:28 -0300

    Update Imagen.py

--- Imagen.py
+++ Imagen.py
@@ -1,20 +1,23 @@
 import pygame
 
-
 ventana=pygame.display.set_mode((800,600))
 clock=pygame.time.Clock()
 
 done=False
 
 imagen=pygame.image.load("labim.jpg")
+imagen = pygame.transform.scale(imagen, (800,600))
 while not done:
     for event in pygame.event.get():
        if event.type==pygame.QUIT:
            done=True
     
-    ventana.blit(imagen, [250,150])
+    ventana.blit(imagen, [0,0])
 
     pygame.display.flip()
     clock.tick(60)
+
+done=False
+
 
 pygame.quit()
