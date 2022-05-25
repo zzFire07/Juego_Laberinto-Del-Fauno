ec96db94e9722c5e2d83e81248fa7578175acd7d
Giorgio.S <104432340+zzFire07@users.noreply.github.com>
2022-05-25 09:15:21 -0300

    Giorgio_avances_V04.py

--- giorgio_practica.py
+++ giorgio_practica.py
@@ -2,79 +2,17 @@ import pygame, os
 pygame.init()
 
 
-def movimiento_jugador(keys_pressed, jugador):
-    if keys_pressed[pygame.K_a] or keys_pressed[pygame.K_LEFT]: #SI SE APRETA A O FLECHA IZQUIERDA SE MUEVE 3 PIXELES HACÍA AHÍ
+def movimiento_jugador(keys_pressed, jugador): #Funcion para el movimiento del jugador
+    if keys_pressed[pygame.K_a] or keys_pressed[pygame.K_LEFT]: #SI SE APRETA A O FLECHA IZQUIERDA SE MUEVE 4 PIXELES HACÍA LA IZQUIERDA
         jugador.x -= vel
-    if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_RIGHT]: #SI SE APRETA D O FLECHA DERECHA SE MUEVE 3 PIXELES HACÍA AHÍ
+    if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_RIGHT]: #SI SE APRETA D O FLECHA DERECHA SE MUEVE 4 PIXELES HACÍA LA DERECHA
         jugador.x += vel
-    if keys_pressed[pygame.K_w] or keys_pressed[pygame.K_UP]: #SI SE APRETA W O FLECHA SUPERIOR SE MUEVE 3 PIXELES HACÍA AHÍ
+    if keys_pressed[pygame.K_w] or keys_pressed[pygame.K_UP]: #SI SE APRETA W O FLECHA SUPERIOR SE MUEVE 4 PIXELES HACÍA ARRIBA
         jugador.y -= vel
-    if keys_pressed[pygame.K_s] or keys_pressed[pygame.K_DOWN]: #SI SE APRETA S O FLECHA INFERIOR SE MUEVE 3 PIXELES HACÍA AHÍ
+    if keys_pressed[pygame.K_s] or keys_pressed[pygame.K_DOWN]: #SI SE APRETA S O FLECHA INFERIOR SE MUEVE 4 PIXELES HACÍA ABAJO
         jugador.y += vel
 
-
-blanco = (255, 255, 255)
-negro = (0, 0, 0)
-rojo = (255, 0, 0)
-azul = (0, 0, 255)
-
-ancho, alto = 1000, 600
-ventana = pygame.display.set_mode((ancho, alto))
-pygame.display.set_caption("Laberinto del fauno")
-
-vel = 4
-FPS = 60
-
-largo_jugador, alto_jugador = 40, 40
-
-imagen = pygame.image.load("labim.jpg")
-imagen = pygame.transform.scale(imagen, (1000,600))
-
-imagen_jugador = pygame.image.load("stickman.jpg")
-imagen_jugador = pygame.transform.scale(imagen_jugador, (largo_jugador, alto_jugador))
-
-def mov_imagenes(jugador):
-    ventana.blit(imagen, (0, 0))
-    ventana.blit(imagen_jugador, (jugador.x, jugador.y))
-
-    pygame.display.update()
-
-def main():import pygame, os
-pygame.init()
-
-
-def movimiento_jugador(keys_pressed, jugador):
-    if keys_pressed[pygame.K_a] or keys_pressed[pygame.K_LEFT] and jugador.x > 0: #SI SE APRETA A O FLECHA IZQUIERDA SE MUEVE 3 PIXELES HACÍA AHÍ
-        jugador.x -= vel
-    if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_RIGHT] and jugador.x + largo_jugador < 1000: #SI SE APRETA D O FLECHA DERECHA SE MUEVE 3 PIXELES HACÍA AHÍ
-        jugador.x += vel
-    if keys_pressed[pygame.K_w] or keys_pressed[pygame.K_UP] and jugador.y > 0: #SI SE APRETA W O FLECHA SUPERIOR SE MUEVE 3 PIXELES HACÍA AHÍ
-        jugador.y -= vel
-    if keys_pressed[pygame.K_s] or keys_pressed[pygame.K_DOWN] and jugador.y + alto_jugador < 600: #SI SE APRETA S O FLECHA INFERIOR SE MUEVE 3 PIXELES HACÍA AHÍ
-        jugador.y += vel
-
-
-blanco = (255, 255, 255)
-negro = (0, 0, 0)
-rojo = (255, 0, 0)
-azul = (0, 0, 255)
-
-ancho, alto = 1000, 600
-ventana = pygame.display.set_mode((ancho, alto))
-pygame.display.set_caption("Laberinto del fauno")
-
-vel = 4
-FPS = 60
-
-largo_jugador, alto_jugador = 40, 40
-
-imagen = pygame.image.load("labim.jpg")
-imagen = pygame.transform.scale(imagen, (1000,600))
-
-imagen_jugador = pygame.image.load("stickman.jpg")
-imagen_jugador = pygame.transform.scale(imagen_jugador, (largo_jugador, alto_jugador))
-
-def mov_imagenes(jugador):
+def mov_imagenes(jugador): #Funcion para las imagenes en el juego
     ventana.blit(imagen, (0, 0))
     ventana.blit(imagen_jugador, (jugador.x, jugador.y))
 
@@ -89,42 +27,39 @@ def main():
     #PROGRAMA MAIN
     while run == True:
 
-        clock.tick(FPS)
+        clock.tick(FPS) #Se limitan los fotogramas a "FPS"
         ventana.blit(imagen, (0,0))
         ventana.blit(imagen_jugador, (jugador.x, jugador.y))
 
         for event in pygame.event.get():
-            print(event)
-
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #Se terminara el bucle si se cierra la ventana.
                 run = False
 
         keys_pressed = pygame.key.get_pressed()
         movimiento_jugador(keys_pressed, jugador)
         mov_imagenes(jugador)
 
-main()
+#------------------------------------------------------------------------------#
+#COLORES
+blanco = (255, 255, 255)
+negro = (0, 0, 0)
+rojo = (255, 0, 0)
+azul = (0, 0, 255)
 
-    jugador = pygame.Rect(500, 250, largo_jugador, alto_jugador)
+#CONSTANTES
+vel = 4 #Velocidad del jugador
+FPS = 60 #Fotogramas por segundo que tendra el programa
 
-    clock = pygame.time.Clock()
-    run = True
+ancho, alto = 1000, 600 #Dimensiones de la ventana del juego
+ventana = pygame.display.set_mode((ancho, alto))
+pygame.display.set_caption("Laberinto del fauno") #NOMBRE DEL JUEGO
 
-    #PROGRAMA MAIN
-    while run == True:
 
-        clock.tick(FPS)
-        ventana.blit(imagen, (0,0))
-        ventana.blit(imagen_jugador, (jugador.x, jugador.y))
+imagen = pygame.image.load("labim.jpg") #Cargamos la imagen que representara al jugador
+imagen = pygame.transform.scale(imagen, (1000,600))
 
-        for event in pygame.event.get():
-            print(event)
+largo_jugador, alto_jugador = 40, 40 #Dimensiones del jugador
+imagen_jugador = pygame.transform.scale(pygame.image.load("stickman.jpg"), (largo_jugador, alto_jugador))
 
-            if event.type == pygame.QUIT:
-                run = False
-
-        keys_pressed = pygame.key.get_pressed()
-        movimiento_jugador(keys_pressed, jugador)
-        mov_imagenes(jugador)
 
 main()
