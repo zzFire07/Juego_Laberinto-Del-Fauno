1356fc42ae496a6ff806e4b3eedbc003243f35a8
yazminEspagnolo <104398358+yazminEspagnolo@users.noreply.github.com>
2022-06-07 19:05:16 -0300

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,22 @@
+import sys
+import pygame.display
+import pygame
+pygame.init()
+ventana= pygame.display.set_mode((400,300))
+pygame.display.set_caption("Timer")
+Fuente= pygame.font.SysFont("Arial", 40)
+aux=1
+while True:
+    ventana.fill((255, 255, 255))
+    Tiempo= pygame.time.get_ticks()/1000
+    if aux== Tiempo:
+        aux+=1
+        print(Tiempo)
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    contador=Fuente.render("Tiempo: "+str(Tiempo),0,(250,30,0))
+    ventana.blit(contador,(100,100))
+    pygame.display.update()
+
